fda5c02eee1843cb13a1ca1a989331b309e3dcb6
4003102 <40223102@gm.nfu.edu.tw>
2015-06-27 18:48:10 +0800

    gear02_02

--- wsgi.py
+++ wsgi.py
@@ -115,7 +115,7 @@ class Hello(object):
 
     @cherrypy.expose
     # N 為齒數, M 為模數, P 為壓力角
-    def drawspur(self, N=15, N1=24,M=10, P=20):
+    def drawspur(self, N=15, N1=24,N2=15,N3=24,M=10, P=20):
         outstring = '''
     <!DOCTYPE html> 
     <html>
@@ -144,7 +144,23 @@ class Hello(object):
             outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
         outstring+='''
        </select><br/>
-
+    第3齒數:<br />
+        <select name="N2">
+        '''
+        for j in range(15,81):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
+    第4齒數:<br />
+        <select name="N3">
+        '''
+        j=24
+        outstring +=''' <option value = '''+str(j)+'''>'''+str(j)+'''</option>'''
+        
+        for j in range(15,81):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
     模數  :<input type=text name=M value='''+str(M)+'''><br />
 
     壓力角:<input type=text name=P value = '''+str(P)+'''><br />
@@ -165,7 +181,7 @@ class Hello(object):
         return outstring
     @cherrypy.expose
     # N 為齒數, M 為模數, P 為壓力角
-    def drawspuraction(self, N=15, N1=24,M=10, P=20):
+    def drawspuraction(self, N=15, N1=24,N2=15,N3=24,M=10, P=20):
         outstring =''' 
     <!DOCTYPE html> 
     <html>
@@ -216,11 +232,16 @@ class Hello(object):
     n_g1 = '''+str(N)+'''
     # 第2齒輪齒數
     n_g2 = '''+str(N1)+'''
-
+    # 第3齒輪齒數
+    n_g3 ='''+str(N2)+'''
+    # 第4齒輪齒數
+    n_g4 ='''+str(N3)+'''
         
     # 計算兩齒輪的節圓半徑
     rp_g1 = m*n_g1/2
     rp_g2 = m*n_g2/2
+    rp_g3 = m*n_g3/2
+    rp_g4 = m*n_g4/2
 
     # 繪圖第1齒輪的圓心座標
     x_g1 = 400
@@ -229,6 +250,13 @@ class Hello(object):
     x_g2 = x_g1
     y_g2 = y_g1+rp_g1 + rp_g2
 
+    # 第3齒輪的圓心座標
+    x_g3 = x_g2+ rp_g2+rp_g3
+    y_g3 = y_g2
+
+    # 第4齒輪的圓心座標
+    x_g4 = x_g3
+    y_g4 = y_g3 + rp_g3+rp_g4
 
 
     # 將第1齒輪順時鐘轉 90 度
@@ -259,6 +287,33 @@ class Hello(object):
     spur.Spur(ctx).Gear(x_g2, y_g2, rp_g2, n_g2, pa, "black")
     ctx.restore()
 
+    # 將第3齒輪順時鐘轉 90 度
+    # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
+
+    ctx.font = "10px Verdana";
+    ctx.fillText("組員:02",x_g3, y_g3);
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g3, y_g3)
+    # rotate to engage
+    ctx.rotate(-pi/2-pi/n_g3+(pi/2+pi/n_g2)*n_g2/n_g3)
+    # put it back
+    ctx.translate(-x_g3, -y_g3)
+    spur.Spur(ctx).Gear(x_g3, y_g3, rp_g3, n_g3, pa, "red")
+    ctx.restore()
+
+    # 將第4齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g4, y_g4)
+    # rotate to engage
+    ctx.rotate(-pi/n_g4+(-pi/2+pi/n_g3)*n_g3/n_g4-(pi/2+pi/n_g2)*n_g2/n_g4)
+    # put it back
+    ctx.translate(-x_g4, -y_g4)
+    spur.Spur(ctx).Gear(x_g4, y_g4, rp_g4, n_g4, pa, "blue")
+    ctx.restore()
 
     </script>
     <canvas id="plotarea" width="3000" height="3000"></canvas>
